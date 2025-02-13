e13765af49bd900321f6ff15067bc99a751c2de8
elinejk <51362561+elinejk@users.noreply.github.com>
2025-02-13 10:18:03 +0100

    Add files via upload

---
+++ scripts/combine_ss.R
@@ -0,0 +1,26 @@
+#### PACKAGES
+library(data.table)
+library(magrittr)
+
+setDTthreads(2)
+
+#### INIT
+
+# summary statistics
+d_res <- "/home/ekoornstra/sure_data/freeze6/permutation_test"
+chr <- c(1:22, "X")
+f_res <- paste0("out_chr", chr, ".txt.gz")
+p_res <- file.path(d_res, f_res)
+
+# output
+f_out <- "pvalue.freeze7.snp-permutation.04042024.txt.gz"
+p_out <- file.path(d_res, f_out)
+
+#### MAIN
+
+# load
+ss <- lapply(p_res, fread) %>%
+  rbindlist()
+
+# save
+fwrite(ss, p_out, sep = '\t')
