0476e9b70187274d35eeabe3bd5af15dca5ee868
elinejk <51362561+elinejk@users.noreply.github.com>
2025-02-13 13:04:30 +0100

    Update process.sh

--- scripts/processing/process.sh
+++ scripts/processing/process.sh
@@ -6,21 +6,10 @@
 #SBATCH --array=1-22
 #SBATCH --mail-type=END
 #SBATCH --mail-user=e.koornstra@erasmusmc.nl
-#SBATCH --output=/home/ekoornstra/sure_data/freeze6/logs/process_reads-%A_%a.out
+#SBATCH --output=/home/ekoornstra/sure_data/freeze7/logs/process_reads-%A_%a.out
 
 
-#Rscript process_raw_whnpcbr3.R \
-#  ${SLURM_ARRAY_TASK_ID} \
-#  /gpfs/home6/ekoornstra/sure_data/new_processing/results/full/no_ds
-
-#Rscript process_raw_wohnpcbr3.R \
-#  ${SLURM_ARRAY_TASK_ID} \
-#  /gpfs/home6/ekoornstra/sure_data/new_processing/results/full/no_b3
-
-#Rscript process_raw_ubi.R \
-#  ${SLURM_ARRAY_TASK_ID} \
-#  /gpfs/home6/ekoornstra/sure_data/new_processing/results/full/ubiquitous
 
 Rscript process_raw_wohnpcbr3_snp-permutation.R \
   ${SLURM_ARRAY_TASK_ID} \
-  /gpfs/home6/ekoornstra/sure_data/freeze6/permutation_test
+  /gpfs/home6/ekoornstra/sure_data/freeze7/
