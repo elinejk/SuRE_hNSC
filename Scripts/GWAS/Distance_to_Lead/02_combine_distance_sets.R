6b0d68880c04fb876437a196a4e66b971a6bc46d
elinejk <51362561+elinejk@users.noreply.github.com>
2025-02-18 11:11:34 +0100

    Add files via upload

---
+++ Scripts/GWAS/Distance_to_Lead/02_combine_distance_sets.R
@@ -0,0 +1,33 @@
+library(data.table)
+library(dplyr)
+
+### paths
+path <- "/gpfs/home6/ekoornstra/surexgwas/freeze7/distance-to-lead/"
+
+phenos <- c("ADHD", "ASD", "BPD", "CP", "IQ", "MDD", "SCZ")
+set <- "sampled_matched_controls"
+
+a <- data.table()
+
+### make a loop for our method
+for (pheno in phenos){
+	## load an prepare the raQTL file
+	df <- fread(paste0(path, set, "/", pheno, "_", set, "_gwassign_distance-snp-to-lead_16042024.txt"))
+	df$Phenotype <- pheno
+	
+	# combine files
+	a <- rbind(a, df)
+	fwrite(a, paste0(path, set, "/allphenos_", set, "_gwassign_distance-snp-to-lead_16042024.txt"), quote = F, row.names=F, sep='\t')
+	
+	# sort on rsid and shortest distance, then retain the first row
+	b <- a %>%
+	arrange(SNP_ID, distance_lead) %>%
+	distinct(SNP_ID, .keep_all = T)
+	
+	fwrite(b, paste0(path, set, "/allphenos-shortestdist_", set, "_gwassign_distance-snp-to-lead_16042024.txt"), quote = F, row.names=F, sep='\t')
+ 
+        # only keep the ones significant in the GWAS
+        #c <- a[a$GWAS_P <= 5e-8, ]
+  
+        #fwrite(c, paste0(path, set, "/allphenos-gwassign_", set, "_distance-snp-to-lead_16042024.txt"), quote = F, row.names=F, sep='\t')
+}
