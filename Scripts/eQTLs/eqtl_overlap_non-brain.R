64731a5899ece5833301e2bb80b3e6649b83108a
Eline Koornstra <51362561+elinejk@users.noreply.github.com>
2025-10-22 13:36:44 +0200

    Update and rename eqtl_overlap_alternative-pipeline_non-brain_31012024.R to eqtl_overlap_non-brain.R

--- Scripts/eQTLs/eqtl_overlap_non-brain.R
+++ Scripts/eQTLs/eqtl_overlap_non-brain.R
@@ -1,38 +1,41 @@
 library(data.table)
 library(tidyverse)
 
-# paths and settings             
-tissues <- c("Cells_Transformed_fibroblasts","Liver", "Whole_Blood")
+## PATHS AND SETTINGS          
+tissues <- c("Cells_Transformed_fibroblasts", "Liver", "Whole_Blood")
 
 all_path <- "/projects/0/AdamsLab/Projects/sure/resources/eQTLs/"
 sign_path <- "/projects/0/AdamsLab/Projects/sure/resources/eQTLs/"
 
 raqtl_path <- "/gpfs/home6/ekoornstra/raqtls/freeze7/"
-
 anatype <- "np-raqtls"
 
-# prepare raQTL file
+## PREPARE EMVAR OR CONTROL FILE ##
+# emVar
 raqtl <- fread(paste0(raqtl_path, "annotations/hnsc_no_downsampling_snp-permutation_freeze7_wilc-raqtls_04042024_ANNOTATED.txt")) %>% as.data.table()
+
+# control/null variant
 #raqtl <- fread(paste0(raqtl_path, "annotations/hnsc_no_downsampling_snp-permutation_freeze7_controls_04042024_ANNOTATED.txt")) %>% as.data.table()
-
 #raqtl <- raqtl[raqtl$hNPC.wilcoxon.pvalue > 0.5, ]
-raqtl <- raqtl[raqtl$NPC_narrowpeak_DHS == "1", ]
 
+# subset the file
+raqtl <- raqtl[raqtl$NPC_narrowpeak_DHS == "1", ]
 raqtl$chrom <- gsub("chr", "", raqtl$chrom)
 raqtl[, variant_id := paste0(chrom, "_", pos.hg19, "_", ref.seq, "_", alt.seq, "_b37")]
 raqtl <- raqtl[, c("SNP_ID", "variant_id", "hNPC.cDNA.ref.mean", "hNPC.cDNA.alt.mean")]
 
 print(paste0("number of raqtls in file: ", nrow(raqtl)))
 
-# start the loop to get for each tissue the total tested SNPs, significant pairs, and concordance
+## LOOP FOR OVERLAP ##
+# get for each tissue the total tested SNPs, significant pairs, and concordance
 for (tissue in tissues){
   # get total tested SNPs
-  #tested <- fread(paste0(all_path, tissue, ".allpairs.txt.gz")) %>% as.data.table()
-  #ov <- merge(raqtl, tested, by = "variant_id")
-  #a <- ov$SNP_ID %>% as.data.table()
-  #a <- unique(a)
-  #rm(tested)
-  #rm(ov)
+  tested <- fread(paste0(all_path, tissue, ".allpairs.txt.gz")) %>% as.data.table()
+  ov <- merge(raqtl, tested, by = "variant_id")
+  a <- ov$SNP_ID %>% as.data.table()
+  a <- unique(a)
+  rm(tested)
+  rm(ov)
   
   # then determine the significant eqtls
   eqtl <- fread(paste0(sign_path, tissue, ".v7.signif_variant_gene_pairs.txt.gz")) %>% as.data.table()
@@ -53,11 +56,11 @@ for (tissue in tissues){
   d <- unique(d)
   
   # get summary information
-  print(paste0("analysis of raqtls: ", anatype))
+  print(paste0("analysis of emvars or null variants: ", anatype))
   print(paste0("printing summary information for ", tissue))
   #print(paste0("hNSC had ", nrow(a), " controls tested as eQTL for ", tissue))
-  print(paste0(nrow(b), " raqtls were significant eQTLs in ", tissue))
-  print(paste0(nrow(d), " raqtls and significant eQTLs have the same effect direction in ", tissue))
+  print(paste0(nrow(b), " variants were significant eQTLs in ", tissue))
+  print(paste0(nrow(d), " variants and significant eQTLs have the same effect direction in ", tissue))
   print("[[[[]]]]")
   
   # save files
