bcfb356b6a97a0484d3ca8468cadb0c5d7dfc0d4
Eline Koornstra <51362561+elinejk@users.noreply.github.com>
2025-10-22 13:57:30 +0200

    Update and rename 01new_sure_in_gwas_loci.R to 01_sure_in_gwas_loci.R

--- Scripts/GWAS/emVars_in_Loci/01_sure_in_gwas_loci.R
+++ Scripts/GWAS/emVars_in_Loci/01_sure_in_gwas_loci.R
@@ -36,7 +36,7 @@ grsure$SNP_ID <- df$SNP_ID
 
 rm(df)
 
-## STEP 1.3: prepare hNSC raQTLs and gwas sign snps
+## STEP 1.3: prepare hNSC emVars and GWAS significant snps
 raqtl <- fread(paste0(path, hnsc))
 raqtl <- raqtl[, "SNP_ID"]
 
@@ -44,10 +44,9 @@ snps <- fread(paste0(path, snp_file))
 snps <- snps[, "rsID"]
 
 
-
 ### STEP 2: START WITH THE OVERLAPS ###
-## STEP 2.1: sure SNPs within GWAS loci based on the coordinates of the gwas locus, not the actual gwas snps tested
-# 2.1.1: sure snps in the fuma locus file
+## STEP 2.1: SuRE SNPs within GWAS loci based on the coordinates of the GWAS locus, not the actual GWAS SNPs tested
+# 2.1.1: SuRE SNPs in the FUMA locus file
 ov <- findOverlaps(grlocus, grsure)
 ov_locus <- grlocus[queryHits(ov)]
 ov_sure <- grsure[subjectHits(ov)]
@@ -57,8 +56,7 @@ inlocus <- as.data.table(ov_locus)
 names(inlocus)[names(inlocus) == 'seqnames'] <- "chr"
 inlocus <- unique(inlocus)
 
-
-# 2.1.2: load gwas sumstats
+# 2.1.2: load GWAS sumstats
 gwas <- fread(paste0(path, sumstats), fill=TRUE)
 names(gwas)[names(gwas) == 'ID' | names(gwas) == 'MarkerName' | names(gwas) == 'variant_id' | names(gwas) == 'RSID'] <- "SNP"
 names(gwas)[names(gwas) == 'chromosome' | names(gwas) == 'Chromosome' | names(gwas) == 'CHROM'] <- "CHR"
@@ -74,39 +72,34 @@ grgwas$SNP <- gwas$SNP
 grgwas$P <- gwas$P
 
 
-
 # 2.1.3: get some statistics
 # sure snps in locus that were also tested in the gwas
 inlocus <- inlocus %>% mutate(GWAS_SNP = ifelse(inlocus$SNP_ID %in% gwas$SNP, "1", "0"))
 inlocus$GWAS_SNP <- as.numeric(inlocus$GWAS_SNP)
 
-# sure snps in locus that are raQTLs
-inlocus <- inlocus %>% mutate(hNSC_raQTL = ifelse(inlocus$SNP_ID %in% raqtl$SNP_ID, "1", "0"))
-inlocus$hNSC_raQTL <- as.numeric(inlocus$hNSC_raQTL)
+# sure snps in locus that are emVars
+inlocus <- inlocus %>% mutate(hNSC_emVar = ifelse(inlocus$SNP_ID %in% raqtl$SNP_ID, "1", "0"))
+inlocus$hNSC_emVar <- as.numeric(inlocus$hNSC_emVar)
 
-# raQTLs in the locus that were part of the gwas sumstats (sure in locus vs raqtls vs gwas snps)
-inlocus <- inlocus %>% mutate(hNSC_raQTL_GWAS = ifelse(inlocus$SNP_ID %in% raqtl$SNP_ID & inlocus$SNP_ID %in% gwas$SNP, "1", "0"))
-inlocus$hNSC_raQTL_GWAS <- as.numeric(inlocus$hNSC_raQTL_GWAS)
+# emVars in the locus that were part of the gwas sumstats (sure in locus vs emVars vs gwas snps)
+inlocus <- inlocus %>% mutate(hNSC_emVar_GWAS = ifelse(inlocus$SNP_ID %in% raqtl$SNP_ID & inlocus$SNP_ID %in% gwas$SNP, "1", "0"))
+inlocus$hNSC_emVar_GWAS <- as.numeric(inlocus$hNSC_emVar_GWAS)
 
 # sure snps in the locus are "in LD with lead SNPs/indsig" gwas locus SNPs (nSNPs column), named here as candidate
 inlocus <- inlocus %>% mutate(SuRE_GWAScandidate = ifelse(inlocus$SNP_ID %in% snps$rsID, "1", "0"))
 inlocus$SuRE_GWAScandidate <- as.numeric(inlocus$SuRE_GWAScandidate)
 
-# raQTLs are also "in LD with lead SNPs/indsig" locus snps
-inlocus <- inlocus %>% mutate(hNSCraQTL_GWAScandidate = ifelse(inlocus$SNP_ID %in% snps$rsID & inlocus$SNP_ID %in% raqtl$SNP_ID, "1", "0"))
-inlocus$hNSCraQTL_GWAScandidate <- as.numeric(inlocus$hNSCraQTL_GWAScandidate)
+# emVars are also "in LD with lead SNPs/indsig" locus snps
+inlocus <- inlocus %>% mutate(hNSCemVar_GWAScandidate = ifelse(inlocus$SNP_ID %in% snps$rsID & inlocus$SNP_ID %in% raqtl$SNP_ID, "1", "0"))
+inlocus$hNSCemVar_GWAScandidate <- as.numeric(inlocus$hNSCemVar_GWAScandidate)
 
-
-
-# 2.1.4: add gwas pvalues
+# 2.1.4: add GWAS pvalues
 a <- gwas[, c("SNP", "P")]
 names(a) <- c("SNP_ID", "GWAS_P")
 inlocus <- merge(inlocus, a, by = "SNP_ID", all.x=TRUE)
 inlocus <- inlocus %>% mutate(GWAS_sign = ifelse(inlocus$GWAS_P <= 5*10^-8, "1", "0"))
 inlocus$GWAS_sign[is.na(inlocus$GWAS_sign)] <- 0
 inlocus$GWAS_sign <- as.numeric(inlocus$GWAS_sign)
-
-head(inlocus)
 
 
 # 2.1.5: save
@@ -125,11 +118,8 @@ ollocus <- as.data.table(ol_locus)
 ollocus <- ollocus %>% count(locus)
 names(ollocus) <- c("locus", "nGWAS")
 
-
 # 2.2.2: merge this information for the final file
 final_file <- merge(loci, ollocus, by.x="GenomicLocus", by.y="locus", all.x=TRUE)
-
-
 
 
 ### STEP 3: START GENERATION OF THE FINAL SUMMARY FILE ###
@@ -141,25 +131,24 @@ loc <- inlocus[ , -c("SNP_ID", "chr", "start", "end", "width", "strand", "nSNPs"
 
 fullfile <- merge(final_file, a, by.x = "GenomicLocus", by.y = "locus", all.x=TRUE)
 
-
-
 ## STEP 3.2: then get the statistics for the other overlaps
 b <- loc %>% 
   group_by(loc$locus) %>% 
-  summarize(nBoth = sum(GWAS_SNP), nhNSC_raQTL = sum(hNSC_raQTL), nhNSC_raQTL_GWAS = sum(hNSC_raQTL_GWAS),
-            nSuRE_nSNPs = sum(SuRE_GWAScandidate), nhNSC_raQTL_nSNPs = sum(hNSCraQTL_GWAScandidate), nGWASsign_SuRE = sum(GWAS_sign))
-names(b) <- c("GenomicLocus", "nBoth", "nhNSC_raQTL", "nhNSC_raQTL_GWAS", "nSuRE_nSNPs", "nhNSC_raQTL_nSNPs", "nGWASsign_SuRE")
+  summarize(nBoth = sum(GWAS_SNP), nhNSC_emVar = sum(hNSC_emVar), nhNSC_emVar_GWAS = sum(hNSC_emVar_GWAS),
+            nSuRE_nSNPs = sum(SuRE_GWAScandidate), nhNSC_emVar_nSNPs = sum(hNSCemVar_GWAScandidate), nGWASsign_SuRE = sum(GWAS_sign))
+names(b) <- c("GenomicLocus", "nBoth", "nhNSC_emVar", "nhNSC_emVar_GWAS", "nSuRE_nSNPs", "nhNSC_emVar_nSNPs", "nGWASsign_SuRE")
 
 fullfile <- merge(fullfile, b, by = "GenomicLocus", all.x=TRUE)
 
 
 ## STEP 3.3: get a better readable files
 subfile <- fullfile[,c("GenomicLocus", "CHR", "start", "end", "LeadSNPs", "nGWAS", "nSNPs", 
-                       "nSuRE", "nBoth", "nSuRE_nSNPs", "nGWASsign_SuRE", "nhNSC_raQTL", "nhNSC_raQTL_GWAS", "nhNSC_raQTL_nSNPs")]
+                       "nSuRE", "nBoth", "nSuRE_nSNPs", "nGWASsign_SuRE", "nhNSC_emVar", "nhNSC_emVar_GWAS", "nhNSC_emVar_nSNPs")]
 subfile <- unique(subfile)
 
 
 ## STEP 3.4: save file
 fwrite(subfile, paste0(path, outname), quote=F, row.names=F, sep='\t')
+
 
 
