a38bd9b6285149168edc31eafa212066bea56785
Eline Koornstra <51362561+elinejk@users.noreply.github.com>
2025-10-22 13:40:00 +0200

    Update and rename eqtl_overlap_summary-plot_31012024.R to eqtl_overlap_summary-plot.R

--- Scripts/eQTLs/eqtl_overlap_summary-plot.R
+++ Scripts/eQTLs/eqtl_overlap_summary-plot.R
@@ -2,9 +2,11 @@ library(data.table)
 library(ggplot2)
 library(dplyr)
 
-# load file
+## LOAD THE FILE ##
 df <- fread("V:/ddata/CELB/poot/Eline Koornstra/SuRE_general/Results/freeze7/figures/hnsc-raqtls_gtex-v7_overlap_10042024.txt")
 
+## MAKE THE PLOT ##
+# plot settings
 df$Tissue = factor(df$Tissue, levels = c("Amygdala", "Anterior Cingulate Cortex BA24", "Caudate Basal Ganglia",
                                          "Cerebellar Hemisphere", "Cerebellum", "Cortex",
                                          "Frontal Cortex BA9", "Hippocampus", "Hypothalamus", 
@@ -12,35 +14,9 @@ df$Tissue = factor(df$Tissue, levels = c("Amygdala", "Anterior Cingulate Cortex 
                                          "Spinal Cord Cervical c1", "Substantia Nigra",
                                          "Fibroblasts", "Liver", "Whole blood"))
 
-ov_lab <- df[, c("Tissue", "Set", "Significant eQTLs")] %>% group_by(Set) %>% top_n(3) %>% ungroup()
-conc_lab <- df[, c("Tissue", "Set", "Concordance")] %>% group_by(Set) %>% top_n(3) %>% ungroup()
-
 global_size = 14
 
 # make the plot
-pl <- ggplot(df, aes(x=Tissue,y=Set)) +
-  geom_point(aes(size=`Significant eQTLs`, color=Concordance), alpha=I(0.99)) +
-  geom_text(data = ov_lab, aes(x=Tissue,y=Set,label=round(`Significant eQTLs`, 1)), size = 4, nudge_y = 0.5) +
-  geom_text(data = conc_lab, aes(x=Tissue,y=Set,label=round(Concordance, 1), fontface = "italic"), size = 4, nudge_y = 0.3) +
-  theme_minimal() +
-  #scale_size_area(max_size=9) +
-  scale_color_gradientn(colors=colorRampPalette(RColorBrewer::brewer.pal(9,'Reds'))(255)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 1.5, hjust=0),
-        text = element_text(size=14)) +
-  scale_y_discrete(limits = c("hNSC narrowpeak DHS", "hNSC broadpeak DHS", "all hNSC raQTLs"))+
-  scale_x_discrete(position = "top") +
-  scale_size_continuous(range  = c(0.1, 10), 
-                        limits = c(0, 30), 
-                        breaks = c(10, 20, 30)) +
-  labs(x ="GTEx V7 Tissue", y = "hNSC raQTL set", color = " % Concordance", size = "% raQTLs that are significant eQTLs")
-
-pl
-
-ggsave('V:/ddata/CELB/poot/Eline Koornstra/SuRE_general/Results/freeze7/figures/eQTLs/hnsc-raqtls_gtex-v7_overlap_10042024_wtop3labels.pdf', pl,
-       units = "mm", height = 150, width = 350)
-
-
-# different labels
 pl <- ggplot(df, aes(x=Tissue,y=Set)) +
   geom_point(aes(size=`Significant eQTLs`, color=Concordance), alpha=I(0.99)) +
   geom_text(data = df, aes(x=Tissue,y=Set,label=round(`Significant eQTLs`, 1)), size = 4, nudge_y = - 0.3) +
@@ -58,7 +34,6 @@ pl <- ggplot(df, aes(x=Tissue,y=Set)) +
   labs(x ="GTEx V7 Tissue", y = "hNSC raQTL set", color = " % Concordance", size = "% raQTLs that are significant eQTLs") +
   guides(color = guide_colorbar(order = 0), size = guide_legend(order = 1))
 
-pl
-
 ggsave('V:/ddata/CELB/poot/Eline Koornstra/SuRE_general/Results/freeze7/figures/eQTLs/hnsc-raqtls_gtex-v7_overlap_17062024_wlabels.pdf', pl,
        units = "mm", height = 150, width = 350)
+
