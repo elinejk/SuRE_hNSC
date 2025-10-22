52130c8afd1add1366cd2b689d768dcbabbdcb53
Eline Koornstra <51362561+elinejk@users.noreply.github.com>
2025-10-22 15:14:18 +0200

    Update and rename ldsc_barplot_hnsc-raqtls.R to ldsc_phenotype_barplot.R

--- Scripts/GWAS/LDSC/ldsc_phenotype_barplot.R
+++ Scripts/GWAS/LDSC/ldsc_phenotype_barplot.R
@@ -3,12 +3,14 @@ library(dplyr)
 library(data.table)
 library(forcats)
 
+## PATHS AND SETTINGS ##
 pheno <- "cp_lee2018"
 phenotype <- "Cognitive Performance"
 path <- "/gpfs/home6/ekoornstra/ldsc/freeze7/"
 
 results <- fread(paste0(path, "heritability_results/with_controls/", pheno, "_hnsc_heritability_freeze7.results"))
 
+## PREPARE FILE ##
 rows <- c("Coding_UCSCL2_0","Conserved_LindbladTohL2_0","CTCF_HoffmanL2_0", "DGF_ENCODEL2_0","DHS_peaks_TrynkaL2_0",
           "Enhancer_AnderssonL2_0","Enhancer_HoffmanL2_0","FetalDHS_TrynkaL2_0","H3K27ac_HniszL2_0","H3K27ac_PGC2L2_0",
           "H3K4me1_peaks_TrynkaL2_0","H3K4me3_peaks_TrynkaL2_0","H3K9ac_peaks_TrynkaL2_0","Intron_UCSCL2_0",
@@ -37,6 +39,7 @@ results_sub <- results_sub %>%
     TRUE ~ NA_character_
   ))
 
+## MAKE THE PLOT ##
 ldscplot <- ggplot(results_sub, aes(x=Category, y=Enrichment)) +
   geom_bar(stat="identity", color="black", fill = if_else(results_sub$Category == "hnsc_raqtlL2_0", "#56B4E9",
 						if_else(results_sub$Category == "hnsc_controlL2_0", "#56B4E9", "#0072B2")),
@@ -55,38 +58,3 @@ ldscplot
 
 ggsave(paste0(path, "figures/", pheno, "_hnsc_heritability-enr_freeze7_witcontrol.pdf"), ldscplot, units="mm",width=214, height =175)
 
-
-
-
-##### label sanity check #####
-#ggplot(results_sub, aes(x=Category, y=Enrichment)) +
-#  geom_bar(stat="identity", color="black", fill ="#117f80",
-#           position=position_dodge()) +
-#  geom_errorbar(aes(ymin=Enrichment-Enrichment_std_error, ymax=Enrichment+Enrichment_std_error),
-#                width=.2, position=position_dodge(.9)) +
-#  guides(x = guide_axis(angle = 90))+
-#  labs(x="Annotation Category", y="Enrichment (Prop. h2g / Prop. SNPs)")+
-#  theme_bw() +
-#  geom_hline(yintercept=1, linetype="dashed")
-
-##### alternative version #####
-#results_sub <- results_sub %>%
-#  mutate(significance = case_when(
-#    Enrichment_p > 0.05 ~ "> 0.05", Enrichment_p > 0.01 ~ "< 0.05",
-#    Enrichment_p > 0.001 ~"< 0.01", !is.na(Enrichment_p) ~ "< 0.001",
-#    TRUE ~ NA_character_
-#  ))
-#
-#alt_plot <- ggplot(results_sub, aes(x=fct_reorder(new_names, -`Prop._SNPs`), y=Enrichment)) +
-#  geom_point(shape = 21, size = 3, color = "black", aes(fill = significance), stroke =1.1) +
-#  geom_errorbar(aes(ymin=pmax(Enrichment-Enrichment_std_error, 0), ymax=pmax(0,Enrichment+Enrichment_std_error)),
-#                width=.2, position=position_dodge(.9)) +
-#  guides(x = guide_axis(angle = 60))+
-#  labs(x="Annotation Category", y="Enrichment (Prop. h2g / Prop. SNPs)")+
-#  scale_fill_manual("p-values",values=c("< 0.001" ="#00448D","< 0.01"= "#0074D9", 
-#                                        "< 0.05"= "#4192D9","> 0.05"="#bdddf9")) +
-#  theme_bw() +
-#  geom_hline(yintercept=1, linetype="dashed") +
-#  ylim(0, ylimit)
-#
-#ggsave(paste0(path, pheno, "_1kg-approach_", approach, "_heritability_freeze4_altplot.pdf"), alt_plot, units="mm",width=214, height =150)
