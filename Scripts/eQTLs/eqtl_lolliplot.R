20ba4deba5fd3ad172687e1267231f832aa608ae
Eline Koornstra <51362561+elinejk@users.noreply.github.com>
2025-10-22 13:31:05 +0200

    Update eQTL lollipop plot script with emVar

--- Scripts/eQTLs/eqtl_lolliplot.R
+++ Scripts/eQTLs/eqtl_lolliplot.R
@@ -11,9 +11,9 @@ tissue <- "Cells_Transformed_fibroblasts"
 gene <- "ENSG00000142599.13"
 gn <- "RERE"
 
-
 all <- fread(paste0(path_eqtl, tissue, ".v7.signif_variant_gene_pairs.txt.gz"))
 all <- all[all$gene_id == gene, ]
+
 
 ### STEP 2: ADD ANNOTATIONS FOR COLORS ###
 # load sure file
@@ -23,18 +23,16 @@ sure$chrom <- gsub("chr", "", sure$chrom)
 sure[, variant_id := paste0(chrom, "_", pos.hg19, "_", ref.seq, "_", alt.seq, "_b37")]
 sure <- sure[, c("SNP_ID", "variant_id", "hNPC.cDNA.ref.mean", "hNPC.cDNA.alt.mean")]
 
-# load raQTLs
+# load emVars
 raqtls <- fread("/gpfs/home6/ekoornstra/raqtls/freeze7/annotations/hnsc_no_downsampling_snp-permutation_freeze7_wilc-raqtls_04042024_ANNOTATED.txt")
 raqtls[, variant_id := paste0(chrom, "_", pos.hg19, "_", ref.seq, "_", alt.seq, "_b37")]
-
 
 # add color information
 all <- all %>%
 	mutate(tested = case_when(all$variant_id %in% sure$variant_id & !all$variant_id %in% raqtls$variant_id ~ "SuRE_tested", 
-	all$variant_id %in% raqtls$variant_id ~ "raQTL", 
+	all$variant_id %in% raqtls$variant_id ~ "emVar", 
 	!all$variant_id %in% sure$variant_id & !all$variant_id %in% raqtls$variant_id ~ "not SuRE"))
-	
-			
+		
 # add concordance
 all <- merge(all, sure, by = "variant_id", all.x=TRUE)
 all$hnsc_log2_alt_ref <- log2(all$hNPC.cDNA.alt.mean/all$hNPC.cDNA.ref.mean)
@@ -45,16 +43,13 @@ all <- all %>% mutate(concordant = case_when(all$raqtl_dir == all$slope_dir ~ "c
 					!all$raqtl_dir == all$slope_dir & !is.na(all$raqtl_dir) ~ "discordant"))
 
 
-
 ### STEP 3: MAKE THE LOLLIPOP PLOT ###
+# set colors
 all$fill <- ifelse(all$concordant == "concordant", "#6C88C4", ifelse(all$concordant == "discordant", "#E77577", "#FFD872"))
-all$color <- ifelse(all$tested == "SuRE_tested", "#74737A", ifelse(all$tested == "raQTL", "black", "#cccccc"))			
+all$color <- ifelse(all$tested == "SuRE_tested", "#74737A", ifelse(all$tested == "emVar", "black", "#cccccc"))			
 all <- all %>% separate(variant_id, c("chrom", "position", NA, NA, NA), sep= "_", remove=F)	
 
-# save file inbetween to make the plot on local pc
-#fwrite(all, paste0(path_hits, "fibroblasts_eQTLs_RERE_19062024.txt"), sep='\t', row.names=F)
-all <- fread("~/Documents/ErasmusMC/work_files/fibroblasts_eQTLs_RERE_19062024.txt")
-
+# make the GRanges
 snps <- GRanges(seqnames=Rle(as.numeric(all$chrom)), ranges=IRanges(start=as.numeric(all$position), width =1))
 snps$color <- all$fill
 snps$border <- all$color
@@ -66,16 +61,14 @@ features <- GRanges("1", IRanges(c(8412457,8415479,8416160,8418256,8419824,84201
                                  width = c(2723,180,146,720,222,1378,113,161,199,92,162,80,98,99,124,48,104,96,105,125,70,468,184,305),
                                  height = 0.1, fill = "red"))
 
-
 legend <- list(labels = c("Concordant", "Discordant", "Not SuRE tested"),
               fill = c("#6C88C4","#E77577","#FFD872"))
 
-pdf("~/Documents/ErasmusMC/work_files/fibroblasts_RERE_lollipopplot_19062024.pdf")
+pdf("/gpfs/home6/ekoornstra/resources/eQTLs/fibroblasts_RERE_lollipopplot_19062024.pdf")
 lolli <- lolliplot(snps, features, legend=legend,
                    cex=0.5)
 dev.off()
 lolli
-
 
 pdf(paste0(path_hits, "figures/", tissue, "_RERE_lollipopplot_19062024.pdf"), lolli ,units="mm", width = 200, height = 200)
 
